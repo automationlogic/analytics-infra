47a79d149f9750d302a4b8cf27541891aa3670e0
The Combed Thunderclap <clappedthunder@gmail.com>
2020-04-07 20:01:36 +0100

    Simplify vars file

--- infra/vars.tf
+++ infra/vars.tf
@@ -1,43 +1,19 @@
-variable "org_id" {
-  type = string
-}
-
-variable "analytics_project" {
-  type = string
-}
-
-variable "billing_account" {
-  type = string
-}
-
-variable "owner_email" {
-  type = string
-}
-
-variable "region" {
-  type = string
-}
-
+variable "org_id" {}
+variable "analytics_project" {}
+variable "billing_account" {}
+variable "owner_email" {}
+variable "region" {}
+variable "ml_models_bucket" {}
+variable "ml_articles_bucket" {}
 variable "repos" {
   type = set(string)
 }
-
 variable "topics" {
   type = set(string)
 }
-
 variable "subscriptions" {
   type = set(string)
 }
-
 variable "datasets" {
   type = set(string)
-}
-
-variable "ml_models_bucket" {
-  type = string
-}
-
-variable "ml_articles_bucket" {
-  type = string
 }
