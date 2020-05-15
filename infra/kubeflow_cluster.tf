379b94851a87e2ecda6943c681a5092953e6976c
me <augustyn@automationlogic.co.uk>
2020-05-15 11:22:35 +0100

    cluster name change

--- infra/kubeflow_cluster.tf
+++ infra/kubeflow_cluster.tf
@@ -25,7 +25,7 @@ resource "google_container_node_pool" "kubeflow_preemptible_nodes" {
   project    = var.analytics_project
   name       = var.kubeflow_node_pool
   location   = "${var.region}-b"
-  cluster    = google_container_cluster.primary.name
+  cluster    = google_container_cluster.kubeflow.name
   node_count = 3
 
   node_config {
